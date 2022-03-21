d3c90975eca600095d4a1e70c95c10f29e6f1b1b
Amit Kumar <amitkumar8636@gmail.com>
2022-03-21 18:00:34 +0530

    Friends Problem

---
+++ src/main/scala/app/sparkcode/FriendsProblem.scala
@@ -0,0 +1,15 @@
+package app.sparkcode
+
+import org.apache.spark.SparkContext
+
+object FriendsProblem extends App {
+
+  val spark = new SparkContext("local[*]", "FriendsProblem")
+  val file = spark.textFile("data/friendsdata-201008-180523.csv")
+
+  val data = file.map(x => x.split("::")).map(x => (x(2), (x(3).toFloat, 1)))
+  val result = data.reduceByKey((x, y) => (x._1 + y._2, x._2 + y._2))
+  val finalResult = result.map(x=>(x._1,x._2._1/x._2._2))
+  finalResult.foreach(println)
+
+}
